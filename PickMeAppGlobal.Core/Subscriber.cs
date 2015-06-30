f351a10eb1d63b15d3f60d4bfe001d1d21a542a0
Ilya Kazlou <Ilya_Kazlou1@epam.com>
2015-06-30 18:04:23 +0300

    PickMeAppGlobal.Core

---
+++ PickMeAppGlobal.Core/Subscriber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PickMeAppGlobal.Core
+{
+  public class Subscriber
+  {
+    public Guid UserId { get; set; }
+
+    public Guid SubscriberUserId { get; set; }
+
+    public virtual User User { get; set; }
+
+    public string HubName { get; set; }
+
+    public string HubType { get; set; }
+  }
+}
---
+++ PickMeAppGlobal.Core/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PickMeAppGlobal.Core
+{
+  public class User
+  {
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string DriverHubName { get; set; }
+
+    public string PassengerHubName { get; set; }
+
+    public virtual List<Subscriber> Subscribers { get; set; }
+  }
+}
